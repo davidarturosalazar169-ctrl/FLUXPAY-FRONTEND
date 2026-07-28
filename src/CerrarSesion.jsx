1fdf67318f0513050c432473706fe8cb78f59561
DAVID <david.arturo.salazar.169@gmail.com>
2026-07-28 20:24:17 +0000

    Cerrar sesion

--- src/CerrarSesion.jsx
+++ src/CerrarSesion.jsx
@@ -5,10 +5,11 @@ import { useNavigate } from "react-router-dom";
 
 function CerrarSesion(){
     const navigate = useNavigate();
+  const API_URL = import.meta.env.VITE_API_URL;
 
   const handleLogout = async () => {
     try {
-      await axios.post("http://127.0.0.1:8000/api/logout", {}, {
+      await axios.post(`${API_URL}/logout`, {}, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`
         }
